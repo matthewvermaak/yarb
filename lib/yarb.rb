8f529fb683cd334e8428af705abf43e9ffe54468
Matthew Vermaak <matthewvermaak@gmail.com>
2010-04-27 00:41:50 -0400

    more cleaning

--- lib/yarb.rb
+++ lib/yarb.rb
@@ -3,9 +3,8 @@ require 'yaml'
 
 module YARB
   def self.load(read_or_string, options = {:binding => TOPLEVEL_BINDING})
-    pass_binding   = options[:binding]
     read_or_string = (read_or_string.respond_to?(:read) ? read_or_string.read : read_or_string.to_s)
    
-    YAML::load(ERB.new(read_or_string).result(pass_binding))
+    YAML::load(ERB.new(read_or_string).result(options[:binding]))
   end
 end
--- yarb.gemspec
+++ yarb.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = "yarb"
-  s.version     = "0.0.3"
+  s.version     = "0.0.4"
   s.author      = "Matthew Vermaak"
   s.email       = "matthewvermaak@gmail.com"
   s.homepage    = "http://github.com/matthewvermaak/yarb"
